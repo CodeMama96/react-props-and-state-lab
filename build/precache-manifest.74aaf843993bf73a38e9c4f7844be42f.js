c991c8cd72788f30028e6ae76e05b0ae093f8c4c
Alicia Weenum <weenum.alicia@gmail.com>
2021-03-14 20:53:37 -0400

    Done.

---
+++ build/precache-manifest.74aaf843993bf73a38e9c4f7844be42f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fdc5d48ea2cfa4a72c58db6a36b1f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293efea0adcca7f0267a",
+    "url": "/static/js/2.0f641361.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0f641361.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90ab5d4cd4e671506663",
+    "url": "/static/js/main.f47ed5ad.chunk.js"
+  },
+  {
+    "revision": "a61e931ced9104ca51b4",
+    "url": "/static/js/runtime-main.abe14ed8.js"
+  }
+]);
